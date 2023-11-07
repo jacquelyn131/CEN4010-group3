1db446324aff840d43c2ace9605e83da6de01c81
Jacquelyn Shelby <jacquelyn@DESKTOP-K97AV81>
2023-11-06 22:43:06 -0500

    added an api endpoint

--- server/index.js
+++ server/index.js
@@ -3,6 +3,13 @@ const express = require("express");
 const PORT = process.env.PORT || 3001;
 const app = express();
 
+// create an endpoint for the /api route
+app.get("/api", (req, res) => {
+    res.json({message: "Hello from server."});
+});
 app.listen(PORT, () => {
     console.log(`Server listening on port ${PORT}`);
 });
+
+
+
