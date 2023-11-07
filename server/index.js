dff12e3a50faa6766be5a56f005d7003f75d8cbf
Jacquelyn Shelby <jacquelyn@DESKTOP-K97AV81>
2023-11-06 22:23:49 -0500

    fixed a bug where port was not capitalized.

--- server/index.js
+++ server/index.js
@@ -1,8 +1,8 @@
 
 const express = require("express");
-const port = process.env.PORT || 3001;
+const PORT = process.env.PORT || 3001;
 const app = express();
 
 app.listen(PORT, () => {
-    console.log(`Server listening on port $PORT`);
+    console.log(`Server listening on port ${PORT}`);
 });
