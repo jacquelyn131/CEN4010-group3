f1eec014ab5f95ba2640598da9968ac62661e99a
Jacquelyn Shelby <jacquelyn@DESKTOP-K97AV81>
2023-11-09 20:43:52 -0500

    add PIctureMessage class

---
+++ server/PictureMessage.js
@@ -0,0 +1,13 @@
+class PictureMessage {
+    // fields
+    fileSize;
+    imageFile;
+    timeStamp;
+    toAddress;
+    fromAddress;
+    // constructor
+    // getters
+    // setters
+    // methods
+}
+module:exports = PictureMessage;
